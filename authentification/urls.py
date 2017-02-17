ce6180e91153f97cfe38c779cab432736b3eb190
cocotagada <cjgs@free.fr>
2017-02-17 17:52:52 -0500

    routage

--- authentification/urls.py
+++ authentification/urls.py
@@ -5,12 +5,12 @@ from authentification import views
 
 urlpatterns = [
     url(r'^connexion/$', views.connexion, name='connexion'),
-    url(r'^index/', views.index, name='index'),
-    url(r'^resultats/', views.resultats, name='resultats'),
-    url(r'^deconnexion/', views.deconnexion, name='deconnexion'),
-    url(r'^accounts/', include('registration.backends.hmac.urls')),
-    url(r'^register/', include('django.contrib.auth.urls')),
-    url(r'^creercompte/', views.creercompte),
+    url(r'^index/$', views.index, name='index'),
+    url(r'^resultats/$', views.resultats, name='resultats'),
+    url(r'^deconnexion/$', views.deconnexion, name='deconnexion'),
+    url(r'^accounts/$', include('registration.backends.hmac.urls')),
+    url(r'^register/$', include('django.contrib.auth.urls')),
+    url(r'^creercompte/$', views.creercompte),
 
 
 
