8e83298e7ebc4490666ebb2cbb9e53c266267685
Fabien Cambi <fabien.cambi@enovance.com>
2017-03-03 15:52:01 -0500

    Changed static root to staticfiles

--- authentification/urls.py
+++ authentification/urls.py
@@ -1,8 +1,6 @@
 #-*- coding: utf-8 -*-
 from django.conf.urls import include, url
 from authentification import views
-#from django.conf import settings
-
 
 urlpatterns = [
     url(r'^connexion/$', views.connexion, name='connexion'),
@@ -12,7 +10,5 @@ urlpatterns = [
     url(r'^accounts/$', include('registration.backends.hmac.urls')),
     url(r'^register/$', include('django.contrib.auth.urls')),
     url(r'^creercompte/$', views.creercompte),
-#    url(r'^static/(?P.*)$', include('django.views.static.urls'), {'document_root': settings.STATIC_ROOT}),
 ]
-
 
