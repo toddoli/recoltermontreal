8c77c552ff1401d14d7ee643d33f04b4c757152d
Fabien Cambi <fabien.cambi@enovance.com>
2017-03-03 16:04:14 -0500

    Fixed questionnairequantitatif

--- questionnairequantitatif/urls.py
+++ questionnairequantitatif/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^questionnairequantitatif/(?P<id>\d+)/edit/$', views.questionnairequantitatif_edit, name='questionnairequantitatif_edit'),
     url(r'^questionnairequantitatif/liste/$', views.questionnairequantitatif_list, name='questionnairequantitatif_list'),
     url(r'^questionnairequantitatif/(?P<id>\d+)/lire/$', views.questionnairequantitatif_read, name='questionnairequantitatif_read'),
+    url(r'^questionnairequantitatif/$', views.questionnairequantitatif_new, name='questionnairequantitatif_new'),
 
 
 
